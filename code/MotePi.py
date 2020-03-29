3a46982aea8de2269b7156953bd30a22b4ee0e81
Tim Richardson <software@potton.me.uk>
2020-03-29 12:38:34 +0100

    Return to latest update

--- code/MotePi.py
+++ code/MotePi.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
-from signal import pause
-
 import motepi_patterns as mqtt_handler
 import mqtt_config
 import mqtt_messages
+from signal import pause
 
-mqtthandle = mqtt_messages.Messages(mqtt_config)
 
-handlerclass = mqtt_handler.MotePiPatterns(mqtthandle, "MoteControl")
-handlerclass.start()
+def main():
+    mqtthandle = mqtt_messages.Messages(mqtt_config)
 
-#mqtthandle.setqueuepayload("MoteControl", "BilgeTank", "{}")
-pause()
+    handlerclass = mqtt_handler.MotePiPatterns(mqtthandle, "MoteControl")
+    handlerclass.start()
+
+    pause()
+
+
+if __name__ == "__main__":
+    main()
+
+print("MotePi: Shouldn't get here!")
